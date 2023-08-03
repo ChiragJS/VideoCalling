4257eaf319de311b539260c386f0c6458f94de7f
Chirag <chiragjaiswal2403@gmail.com>
2023-08-03 13:36:40 +0530

    new update

--- client/src/components/Video.jsx
+++ client/src/components/Video.jsx
@@ -1,19 +1,17 @@
 import { useEffect, useRef } from "react";
 
- const Video = ({stream,name}) => {
+const Video = ({stream}) => {
     const videoRef = useRef();
     useEffect(() => {
         if (videoRef && videoRef.current) {
             videoRef.current.srcObject = stream;
-            console.log(name);
         }
       }, [videoRef,stream])
     
       return (
         <div>
           <div>
-            {console.log(stream,name)}
-            <video style={{borderRadius: 10}} ref={videoRef} muted width="100%" autoPlay={true} playsInline={true} />
+            <video style={{borderRadius: 10}} ref={videoRef}  width="100%" autoPlay={true} playsInline={true} />
           </div>
         </div>
       )
